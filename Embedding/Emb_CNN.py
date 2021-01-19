3dfba3ba806ad2dbd759a2410b518a0a737147c5
Uspectacle <57331115+Uspectacle@users.noreply.github.com>
2021-01-19 16:00:34 +0100

    Update Emb_CNN.py

--- Embedding/Emb_CNN.py
+++ Embedding/Emb_CNN.py
@@ -9,31 +9,6 @@ Si auccun choix n'est fait, la convolution s'effectue sur l'ensemble des images 
 import torch
 import torch.nn as nn
 
-class TimeDistributed(nn.Module):
-    def __init__(self, module, batch_first=False):
-        super(TimeDistributed, self).__init__()
-        self.module = module
-        self.batch_first = batch_first
-
-    def forward(self, x):
-
-        if len(x.size()) <= 2:
-            return self.module(x)
-
-        # Squash samples and timesteps into a single axis
-        x_reshape = x.contiguous().view(-1, x.size(-1))  # (samples * timesteps, input_size)
-
-        y = self.module(x_reshape)
-
-        # We have to reshape Y
-        if self.batch_first:
-            y = y.contiguous().view(x.size(0), -1, y.size(-1))  # (samples, timesteps, output_size)
-        else:
-            y = y.view(-1, x.size(1), y.size(-1))  # (timesteps, samples, output_size)
-
-        return y
-
-
 
 class CNNEncoder(nn.Module):
     """docstring for ClassName"""
@@ -45,10 +20,11 @@ class CNNEncoder(nn.Module):
                     
         super(CNNEncoder, self).__init__()
 
-        self.input_reshape = input_shape[1:len(input_shape)]
+        self.input_reshape = list(input_shape[1:len(input_shape)])
         self.dim_concat = dim_concat
         if dim_concat != None:
             self.input_reshape[dim_concat] = self.input_reshape[dim_concat]*input_shape[0]
+        self.input_reshape=tuple(self.input_reshape)
         
         self.TimeDistributed = TimeDistributed
 
@@ -75,7 +51,7 @@ class CNNEncoder(nn.Module):
                         nn.ReLU())
 
     def forward(self,x):
-        x_reshape = x.contiguous().view(-1, self.input_reshape)
+        x_reshape = x.contiguous().view((-1,) + (self.input_reshape))
         y = self.layer1(x_reshape)
         y = self.layer2(y)
         y = self.layer3(y)
@@ -84,3 +60,7 @@ class CNNEncoder(nn.Module):
         if self.TimeDistributed:
             y = y.contiguous().view(x.size(0), -1, y.size(-1))
         return y
+
+from torchsummary import summary
+model = CNNEncoder((100, 3, 50, 50), dim_concat=2, TimeDistributed = False)
+summary(model.cuda(),(100, 3, 50, 50))
